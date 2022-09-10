85be3d29a6ee5f4ee6f4f48b935de4e95b003a39
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-09-11 01:40:56 +0300

    lib takes care of a lot now

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,20 @@
-import {
-  createChannel,
-  createClient,
-  ChannelCredentials,
-  Channel,
-} from "nice-grpc";
+import { Channel } from "nice-grpc";
 import { Message } from "discord.js";
 import fetch from "node-fetch";
 import {
-  GlobalStateServiceClient,
-  GlobalStateServiceDefinition,
-} from "@andreivcodes/spacemeshlib/lib/src/proto/dist/spacemesh/v1/global_state";
-import {
-  AccountDataFilter,
-  AccountDataFlag,
-  AccountDataQueryRequest,
-  AccountDataQueryResponse,
-} from "@andreivcodes/spacemeshlib/lib/src/proto/dist/spacemesh/v1/global_state_types";
-import {
-  TransactionServiceClient,
-  TransactionServiceDefinition,
-} from "@andreivcodes/spacemeshlib/lib/src/proto/dist/spacemesh/v1/tx";
-import { SubmitTransactionResponse } from "@andreivcodes/spacemeshlib/lib/src/proto/dist/spacemesh/v1/tx_types";
-import { AccountId } from "@andreivcodes/spacemeshlib/lib/src/proto/dist/spacemesh/v1/types";
-import {
   toHexString,
-  signTransaction,
   derivePrivateKey,
   derivePublicKey,
 } from "@andreivcodes/spacemeshlib/lib/src/crypto";
+import {
+  createGlobalStateChannel,
+  getAccountBalance,
+  getAccountNonce,
+} from "@andreivcodes/spacemeshlib/lib/src/global_state";
+import {
+  createTransactionChannel,
+  submitTransaction,
+} from "@andreivcodes/spacemeshlib/lib/src/tx";
 
 const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
@@ -36,7 +23,7 @@ require("dotenv").config();
 
 const senderSeed: string = process.env.SEEDPHRASE!;
 let url = "https://discover.spacemesh.io/networks.json";
-let networkUrl: String;
+let networkUrl: string;
 let channel: Channel;
 
 async function main() {
@@ -75,10 +62,6 @@ async function getNetwork() {
     .then((response) => response.json())
     .then((res: any) => {
       networkUrl = res[0]["grpcAPI"].slice(0, -1).substring(8);
-      channel = createChannel(
-        `${networkUrl}:443`,
-        ChannelCredentials.createSsl()
-      );
     });
 }
 
@@ -94,95 +77,45 @@ async function sendSmesh({
   let sk = (await derivePrivateKey(senderSeed, 0)) as Uint8Array;
   let pk = (await derivePublicKey(senderSeed, 0)) as Uint8Array;
 
-  channel = createChannel(`${networkUrl}:443`, ChannelCredentials.createSsl());
-
   console.log(`Connecting to ${networkUrl}:443`);
-  const accountClient: GlobalStateServiceClient = createClient(
-    GlobalStateServiceDefinition,
-    channel
+
+  createGlobalStateChannel(networkUrl, 443, true);
+
+  let accountNonce = await getAccountNonce(pk);
+  let accountBalance = await getAccountBalance(pk);
+
+  console.log(
+    `Tap currently running on address 0x${toHexString(
+      pk
+    )} with nonce ${accountNonce} and has a balance of ${accountBalance} SMD`
   );
 
-  const accountQueryId: AccountId = { address: pk };
+  if (Number(accountNonce) == 0) {
+    message.reply(`My counter is 0... is this the first transaction?`);
+    return;
+  }
+  if (Number(accountBalance) < amount) {
+    message.reply(`I am out of funds :(`);
+    return;
+  }
 
-  const accountQueryFilter: AccountDataFilter = {
-    accountId: accountQueryId,
-    accountDataFlags: AccountDataFlag.ACCOUNT_DATA_FLAG_ACCOUNT,
-  };
-
-  const accountQuery: AccountDataQueryRequest = {
-    filter: accountQueryFilter,
-    maxResults: 1,
-    offset: 0,
-  };
-
-  await accountClient
-    .accountDataQuery(accountQuery)
-    .then(async (result: AccountDataQueryResponse) => {
-      console.log(result.accountItem[0].accountWrapper);
-      let senderAccountNonce =
-        result.accountItem[0].accountWrapper?.stateProjected?.counter;
-
-      let senderAccountBalance =
-        result.accountItem[0].accountWrapper?.stateProjected?.balance?.value;
-
+  createTransactionChannel(networkUrl, 443, true);
+  submitTransaction(Number(accountNonce), to, 1, 1, 100, sk)
+    .then((response) => {
+      message.reply(
+        `just 💸  transferred funds to ${
+          message.content
+        }. \nTx ID: 0x${toHexString(response.txstate?.id?.id!)}`
+      );
       console.log(
-        `Tap currently running on address 0x${toHexString(
-          pk
-        )} with nonce ${senderAccountNonce} and has a balance of ${senderAccountBalance} SMD`
+        `just 💸  transferred funds to ${
+          message.content
+        }. \nTx ID: 0x${toHexString(response.txstate?.id?.id!)}`
       );
-
-      if (Number(senderAccountNonce) == 0) {
-        message.reply(`My counter is 0... is this the first transaction?`);
-        return;
-      }
-      if (Number(senderAccountBalance) < amount) {
-        message.reply(`I am out of funds :(`);
-        return;
-      }
-
-      let tx = await signTransaction({
-        accountNonce: Number(senderAccountNonce),
-        receiver: to,
-        gasLimit: 1,
-        fee: 1,
-        amount: amount,
-        secretKey: toHexString(sk),
-      });
-
-      const transactionClient: TransactionServiceClient = createClient(
-        TransactionServiceDefinition,
-        channel
-      );
-
-      transactionClient
-        .submitTransaction({
-          transaction: tx as Uint8Array,
-        })
-        .then((response: SubmitTransactionResponse) => {
-          if (response.status?.code == 0) {
-            message.reply(
-              `just 💸  transferred funds to ${
-                message.content
-              }. \nTx ID: 0x${toHexString(response.txstate?.id?.id!)}`
-            );
-            console.log(
-              `just 💸  transferred funds to ${
-                message.content
-              }. \nTx ID: 0x${toHexString(response.txstate?.id?.id!)}`
-            );
-          } else
-            message.reply(
-              `could not transfer :( ${JSON.stringify(response.status)}`
-            );
-        })
-        .catch((e: any) => {
-          message.reply(`could not transfer :( submitTransaction failed`);
-          console.log(e);
-        });
     })
-    .catch((e: any) => {
-      message.reply(`could not transfer :( accountDataQuery failed`);
-      console.log(e);
+    .catch((err) => {
+      message.reply(`could not transfer :( submitTransaction failed`);
+      console.log(err);
     });
 }
 
